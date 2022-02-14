62000f1ba3d8779b21824a27f48121639ec737c9
ZimbaCimba <48441531+ZimbaCimba@users.noreply.github.com>
2022-02-14 19:29:33 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+<script>
+    window.onscroll = () => {
+        if (window.srollY>700){
+            document.querySelector('.isShowBtn').classList.remove('isShowBtn_hide')
+        }
+}
+    
+</script>
